9b56b18f1ce2a16268f3f9a215d1eb20c26bb2ef
Oksana <misssevenka@gmail.com>
2019-10-22 15:16:10 +0300

    Add basic app structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Navigation from "./components/Navigation";
+import Home from './components/Home';
+import Favorites from './components/Favorites';
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Container fluid>
+        <Navigation />
+
+        <Switch>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { withRouter } from "react-router";
+import { Nav } from 'react-bootstrap';
+import '../css/Navigation.css';
+
+class Navigation extends Component {
+  navigate(path) {
+    this.props.history.push(path);
+  }
+
+  render() {
+    return (
+      <Nav
+        variant="pills"
+        activeKey={this.props.history.location.pathname}
+        onSelect={selectedKey => this.navigate(selectedKey)}
+        className="navigation">
+        <h1>Herolo Weather Task</h1>
+
+        <div className="links">
+          <Nav.Item>
+            <Nav.Link eventKey="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="/favorites">Favorites</Nav.Link>
+          </Nav.Item>
+        </div>
+      </Nav>
+    );
+  }
+}
+
+export default withRouter(Navigation);

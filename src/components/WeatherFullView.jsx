6b441b877894e777902559d958fc6c9d4f52ce43
Oksana <misssevenka@gmail.com>
2019-10-23 17:08:46 +0300

    Add removing item from main screen

--- src/components/WeatherFullView.jsx
+++ src/components/WeatherFullView.jsx
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
   getFiveDaysForecast,
-  addToFavorites
+  addToFavorites,
+  removeFromFavorites
 } from '../actions';
 import { Card, Media, Button } from 'react-bootstrap';
 import WeatherSmallView from './WeatherSmallView';
 import '../css/WeatherFullView.css';
 
 class WeatherFullView extends Component {
-  onAddToFavorites() {
-    this.props.addToFavorites(this.props.currentLocation);
-  }
-
   componentDidMount() {
     this.props.getFiveDaysForecast(this.props.currentLocation.Key);
   }
@@ -21,6 +18,7 @@ class WeatherFullView extends Component {
     const todaysForecast = this.props.fiveDaysForecast[0];
     const degreesMin = todaysForecast ? `${todaysForecast.Temperature.Minimum.Value}°${todaysForecast.Temperature.Minimum.Unit}` : '';
     const degreesMax = todaysForecast ? `${todaysForecast.Temperature.Maximum.Value}°${todaysForecast.Temperature.Maximum.Unit}` : '';
+    const isLocationInFavorites = this.props.favoriteLocations.some(item => item.Key === this.props.currentLocation.Key);
 
     return (
       <Card className="weather-full-view">
@@ -33,7 +31,10 @@ class WeatherFullView extends Component {
               </p>
             </Media.Body>
           </Media>
-          <Button onClick={() => this.onAddToFavorites()}>Add to favorites</Button>
+          {isLocationInFavorites ?
+            <Button onClick={() => this.props.removeFromFavorites(this.props.currentLocation.Key)} variant="danger">Remove from favorites</Button>
+          :
+            <Button onClick={() => this.props.addToFavorites(this.props.currentLocation)}>Add to favorites</Button>}
         </div>
 
         <div className="inner">
@@ -52,12 +53,14 @@ class WeatherFullView extends Component {
 
 const mapStateToProps = state => ({
   currentLocation: state.weather.currentLocation,
-  fiveDaysForecast: state.weather.fiveDaysForecast
+  fiveDaysForecast: state.weather.fiveDaysForecast,
+  favoriteLocations: state.weather.favoriteLocations
 });
 
 const mapDispatchToProps = {
   getFiveDaysForecast,
-  addToFavorites
+  addToFavorites,
+  removeFromFavorites
 };
 
 export default connect(
